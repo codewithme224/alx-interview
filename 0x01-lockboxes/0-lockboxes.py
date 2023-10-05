4f7d3d67f0c63366447755d212326de0c6460463
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-05 09:01:16 +0000

    Task added

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""method that determines if all the boxes can be opened."""
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    keys = [0]
+    for key in keys:
+        for newKey in boxes[key]:
+            if newKey not in keys and newKey < len(boxes):
+                keys.append(newKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
