aee7b76bf5130c789228884c8f230740502be15d
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-22 16:57:19 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,34 +5,35 @@
 import sys
 
 
-def print_stats(status_codes, file_size):
-    """Prints the stats"""
-    print("File size: {}".format(file_size))
-    for key in sorted(status_codes.keys()):
-        if status_codes[key] > 0:
-            print("{}: {}".format(key, status_codes[key]))
+if __name__ == '__main__':
 
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-file_size = 0
-count = 0
-try:
-    for line in sys.stdin:
-        count += 1
-        data = line.split()
-        try:
-            file_size += int(data[-1])
-        except ValueError:
-            pass
-        try:
-            status_codes[data[-2]] += 1
-        except KeyError:
-            pass
-        if count == 10:
-            print_stats(status_codes, file_size)
-            count = 0
-    print_stats(status_codes, file_size)
-except KeyboardInterrupt:
-    print_stats(status_codes, file_size)
-    raise
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
